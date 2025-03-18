6bf6a79ee23122ce397a69c6c0034be38e2676d8
Reinaldo lira de Sousa <Reinaldolira@academicos.com>
2025-03-18 20:00:10 -0300

    add função do controller autenticação

--- PrimeiraAulaBackAvancado-main/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ PrimeiraAulaBackAvancado-main/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -16,11 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
        var token = new  UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var token = UsernamePasswordAuthenticationToken();
-        var authentication = manager.authenticate(token);
+        manager.authenticate(token);
+        return ResponseEntity.ok.build();
 
     }
 }
+/**
+ var token = UsernamePasswordAuthenticationToken();
+ var authentication = manager.authenticate(token);
+ */
