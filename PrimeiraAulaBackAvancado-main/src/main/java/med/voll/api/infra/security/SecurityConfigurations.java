ebcbc39e688fb973e9f93fab6ce6dbe5f3a628d4
Reinaldo lira de Sousa <Reinaldolira@academicos.com>
2025-03-18 21:04:25 -0300

    add criptografia na senha

--- PrimeiraAulaBackAvancado-main/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ PrimeiraAulaBackAvancado-main/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -25,4 +25,9 @@ public class SecurityConfigurations {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
     return configuration.getAuthenticationManager();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
